d89bd278fd260ff455927f65a7622b51f0920336
STSonyThomas <goodtobetrue2@gmail.com>
2023-05-19 10:47:06 +0530

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,51 @@
+// for splash screen
+
+const splash = document.querySelector(".splash");
+splash.addEventListener("click", () => {
+  splash.style.opacity = "0";
+  setTimeout(() => {
+    splash.classList.add("hidden");
+  }, 1000);
+});
+setTimeout(() => {
+  splash.style.opacity = "0";
+  splash.classList.add("hidden");
+},13000);
+
+
+
+
+const seconds = document.querySelector(".seconds .number"),
+  minutes = document.querySelector(".minutes .number"),
+  hours = document.querySelector(".hours .number"),
+  days = document.querySelector(".days .number");
+
+let secValue = 11,
+  minValue = 2,
+  hourValue = 2,
+  dayValue = 9;
+
+const timeFunction = setInterval(() => {
+  secValue--;
+
+  if (secValue === 0) {
+    minValue--;
+    secValue = 60;
+  }
+  if (minValue === 0) {
+    hourValue--;
+    minValue = 60;
+  }
+  if (hourValue === 0) {
+    dayValue--;
+    hourValue = 24;
+  }
+
+  if (dayValue === 0) {
+    clearInterval(timeFunction);
+  }
+  seconds.textContent = secValue < 10 ? `0${secValue}` : secValue;
+  minutes.textContent = minValue < 10 ? `0${minValue}` : minValue;
+  hours.textContent = hourValue < 10 ? `0${hourValue}` : hourValue;
+  days.textContent = dayValue < 10 ? `0${dayValue}` : dayValue;
+}, 1000); //1000ms = 1s
