45d5fe6679c093fa2afa845e2907ba74fd22b936
H4K3R13 <arjunmundamani@gmail.com>
2023-05-23 23:15:21 +0530

    Styled

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ const e = require("express");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.use("/css", express.static(__dirname + 'public/css'));
-app.use("/images", express.static(__dirname + 'public/images'));
-app.use("/js", express.static(__dirname + 'public/js'));
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
